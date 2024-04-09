082d131b5b9563720aefe8f604a65e2a759578fb
Nick088 <91847579+Nick088Official@users.noreply.github.com>
2024-04-09 23:49:15 +0200

    added seed option

--- Python_Scripts/minecraft-skins.py
+++ Python_Scripts/minecraft-skins.py
@@ -58,7 +58,7 @@ def restore_skin_alphachannels(image):
     
     return converted_image
 
-def main(prompt, num_inference_steps, guidance_scale, num_images_per_prompt, model_precision_type, filename, logger):
+def main(prompt, num_inference_steps, guidance_scale, num_images_per_prompt, model_precision_type, seed, filename, logger):
     # Enable GPU acceleration frameworks, if enabled.
 
     if model_precision_type == "fp16":
@@ -96,7 +96,8 @@ def main(prompt, num_inference_steps, guidance_scale, num_images_per_prompt, mod
         height=768,
         width=768,
         guidance_scale=guidance_scale,
-        num_images_per_prompt=num_images_per_prompt
+        num_images_per_prompt=num_images_per_prompt,
+        seed=seed
     ).images[0]
 
     # Extract and scale down the Minecraft skin portion of the image.
@@ -125,7 +126,8 @@ if __name__ == "__main__":
     parser.add_argument('guidance_scale', type=float, help='How closely the generated image adheres to the prompt')
     parser.add_argument('num_images_per_prompt', type=int, help='The number of images to make with the prompt')
     parser.add_argument('model_precision_type', type=str, help='The precision type to load the model, like fp16 which is faster, or fp32 which gives better results')
-    parser.add_argument('filename', type=str, help='Name of the generated Minecraft skin file')
+    parser.add_argument('seed', type=int, help='A starting point to initiate the generation process')
+    parser.add_argument('filename', type=str, help='Name of the output generated Minecraft skin file')
     parser.add_argument('--verbose', help='Produce verbose output while running', action='store_true', default=False)
 
     args = parser.parse_args()
@@ -137,9 +139,10 @@ if __name__ == "__main__":
     guidance_scale = args.guidance_scale
     num_images_per_prompt = args.num_images_per_prompt
     model_precision_type = args.model_precision_type
+    seed = args.seed
     
     if verbose:
         logger.setLevel(logging.INFO)
     
-    main(prompt, num_inference_steps, guidance_scale, num_images_per_prompt, model_precision_type, filename, logger)
+    main(prompt, num_inference_steps, guidance_scale, num_images_per_prompt, model_precision_type, seed, filename, logger)
     
