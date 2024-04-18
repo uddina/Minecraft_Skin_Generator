d1d30fe13b7d9ae5acd16609158721e27dd411f4
Nick088 <91847579+Nick088Official@users.noreply.github.com>
2024-04-18 21:57:38 +0200

    Update Run_Inference_NO_UI.bat

--- Run_Inference_NO_UI.bat
+++ Run_Inference_NO_UI.bat
@@ -1,4 +1,37 @@
 @echo off
+setlocal
+
+REM Check if venv_no_ui folder exists in the root directory
+if not exist "%~dp0venv_no_ui\" (
+    REM Print the step to the terminal
+    echo Creating virtual environment...
+
+    REM Create a virtual environment named "venv_no_ui" in the root directory
+    python -m venv "%~dp0venv_no_ui"
+)
+
+REM Print the step to the terminal
+echo Activating the virtual environment...
+
+REM Activate the virtual environment
+call "%~dp0venv_no_ui\Scripts\activate.bat"
+
+REM Check if the required packages are already installed
+echo Checking if required packages are installed...
+pip show gradio >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Required packages are already installed. Skipping requirements installation.
+) else (
+    REM Print the step to the terminal
+    echo Installing requirements...
+
+    REM Install requirements inside the virtual environment
+    pip install -r "%~dp0Python_Scripts\requirements_no_ui.txt"
+)
+
+REM Print the step to the terminal
+echo Running Python script...
+
 
 set /p prompt="Enter your prompt: "
 set /p sd_model="Enter the Stable Diffusion Model Version (2/xl, xl understands better prompts): "
@@ -26,4 +59,15 @@ if /i "%verbose%"=="y" (
 
 python Python_Scripts/%sd_model_version%.py "\"%prompt%\"" %num_inference_steps% %guidance_scale% %model_precision_type% %seed% "%filename%" %verbose_flag%
 
-pause /K
+REM Print the step to the terminal
+echo Deactivating the virtual environment...
+
+REM Deactivate the virtual environment
+call "%~dp0venv_no_ui\Scripts\deactivate.bat"
+
+REM Print the step to the terminal
+echo Script execution complete.
+
+endlocal
+
+pause /k
