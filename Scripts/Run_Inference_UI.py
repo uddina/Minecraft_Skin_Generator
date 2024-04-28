1114c61c664c642d073322396e8ae34f77bf1fd4
Nick088 <91847579+Nick088Official@users.noreply.github.com>
2024-04-28 23:56:05 +0200

    Update Run_Inference_UI.py

--- Scripts/Run_Inference_UI.py
+++ Scripts/Run_Inference_UI.py
@@ -3,6 +3,7 @@ import os
 import gradio as gr
 import torch
 from PIL import Image, ImageEnhance
+import spaces
 
 
 if torch.cuda.is_available():
@@ -13,22 +14,38 @@ else:
     print("Using CPU")
 
 
-def run_inference(prompt, stable_diffusion_model, num_inference_steps, guidance_scale, model_precision_type, seed, output_image_name, verbose):
-    
+subprocess.run(["git", "clone", "https://github.com/Nick088Official/Stable_Diffusion_Finetuned_Minecraft_Skin_Generator.git"])
+
+os.chdir("Stable_Diffusion_Finetuned_Minecraft_Skin_Generator")
+
+
+@spaces.GPU()
+def run_inference(prompt, stable_diffusion_model, num_inference_steps, guidance_scale, model_precision_type, seed, filename, verbose, see_in_3d):
+
+    # inference
     if stable_diffusion_model == '2':
         sd_model = "minecraft-skins"
     else:
         sd_model = "minecraft-skins-sdxl"
 
-    inference_command = f"python Scripts/{sd_model}.py '{prompt}' {num_inference_steps} {guidance_scale} {model_precision_type} {seed} {output_image_name} {'--verbose' if verbose else ''}"
+    inference_command = f"python Scripts/{sd_model}.py '{prompt}' {num_inference_steps} {guidance_scale} {model_precision_type} {seed} {filename} {'--verbose' if verbose else ''}"
     
     os.system(inference_command)
-    
-    # Return the generated image and the processed model
-    return os.path.join(f"output_minecraft_skins/{output_image_name}")
 
-    
-    
+    # view it in 3d or not
+    if see_in_3d:
+        from pygltflib import GLTF2
+        from pygltflib.utils import ImageFormat, Texture, Material, Image as GLTFImage
+        os.chdir("Scripts")
+        command_3d_model = f"python to_3d_model.py '{filename}'"
+        os.system(command_3d_model)
+        os.chdir("..")
+        glb_path = os.path.join(f"output_minecraft_skins/{filename}_3d_model.glb")
+        return os.path.join(f"output_minecraft_skins/{filename}"), glb_path
+    else:
+        return os.path.join(f"output_minecraft_skins/{filename}"), None
+
+
 # Define Gradio UI components
 prompt = gr.Textbox(label="Your Prompt", info="What the Minecraft Skin should look like")
 stable_diffusion_model = gr.Dropdown(['2', 'xl'], value="xl", label="Stable Diffusion Model", info="Choose which Stable Diffusion Model to use, xl understands prompts better")
@@ -36,7 +53,8 @@ num_inference_steps = gr.Number(label="Number of Inference Steps", precision=0, 
 guidance_scale = gr.Number(minimum=0.1, value=7.5, label="Guidance Scale", info="The number of denoising steps of the image. More denoising steps usually lead to a higher quality image at the cost of slower inference")
 model_precision_type = gr.Dropdown(["fp16", "fp32"], value="fp16", label="Model Precision Type", info="The precision type to load the model, like fp16 which is faster, or fp32 which gives better results")
 seed = gr.Number(value=42, label="Seed", info="A starting point to initiate generation, put 0 for a random one")
-output_image_name = gr.Textbox(label="Output Image Name", info="The name of the file of the output image skin, keep the .png", value="output-skin.png")
+filename = gr.Textbox(label="Output Image Name", info="The name of the file of the output image skin, keep the.png", value="output-skin.png")
+see_in_3d = gr.Checkbox(label="See in 3D", info="View the generated skin as a £D Model", value=True)
 verbose = gr.Checkbox(label="Verbose Output", info="Produce more detailed output while running", value=False)
 
 
@@ -50,10 +68,14 @@ gr.Interface(
         guidance_scale,
         model_precision_type,
         seed,
-        output_image_name,
+        filename,
+        see_in_3d,
         verbose
     ],
-    outputs=gr.Image(label="Generated Minecraft Skin Image Asset"),
+    outputs=[
+        gr.Image(label="Generated Minecraft Skin Image Asset"),
+        gr.Model3D(clear_color=[0.0, 0.0, 0.0, 0.0],  label="3D Model")
+    ],
     title="Minecraft Skin Generator",
-    description="Make AI generated Minecraft Skins by a Finetuned Stable Diffusion Version!<br>Model used: https://github.com/Nick088Official/Stable_Diffusion_Finetuned_Minecraft_Skin_Generator<br>Credits: [Monadical-SAS](https://github.com/Monadical-SAS/minecraft_skin_generator) (Creators of the model), [Nick088](https://linktr.ee/Nick088) (Improving usage of the model), daroche (helping me fix the 3d model texture isue), [Brottweiler](https://gist.github.com/Brottweiler/483d0856c6692ef70cf90bf1a85ce364) (script to fix the 3d model texture, [meew](https://huggingface.co/spaces/meeww/Minecraft_Skin_Generator/blob/main/models/player_model.glb) (Minecraft Player 3d model)",
+    description="Make AI generated Minecraft Skins by a Finetuned Stable Diffusion Version!<br>Model used: https://github.com/Nick088Official/Stable_Diffusion_Finetuned_Minecraft_Skin_Generator<br>Credits: [Monadical-SAS](https://github.com/Monadical-SAS/minecraft_skin_generator) (Creators of the model), [Nick088](https://linktr.ee/Nick088) (Improving usage of the model), daroche (helping me fix the 3d model texture isue), [Brottweiler](https://gist.github.com/Brottweiler/483d0856c6692ef70cf90bf1a85ce364)(script to fix the 3d model texture, [meew](https://huggingface.co/spaces/meeww/Minecraft_Skin_Generator/blob/main/models/player_model.glb) (Minecraft Player 3d model)",
 ).launch(show_api=False, share=False)
